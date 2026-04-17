0fd05b6b09b82fb9df07228b6ad52e779ab3e2de
Naman Kaushik <kaushiknaman68@gmail.com>
2026-04-17 18:41:57 +0530

    LevelOrderTraversal

---
+++ tree/LevelOrderTraversal.java
@@ -0,0 +1,13 @@
+package tree;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        return  result;
+    }
+}
